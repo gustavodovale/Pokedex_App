c41d974d19a03512e0f7cdab05280b1180d0c64d
gustavo <gust.dovale@gmail.com>
2025-04-23 22:53:41 -0300

    finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,142 +9,123 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 212, 5, 5)),
       ),
-      home: const MyHomePage(title: 'Pokedex'),
+      home: MyHomePage(title: 'Pokedex'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({super.key, required this.title});
 
   final String title;
+  final TextEditingController controller = TextEditingController();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String nome = '';
+  String tipos = '';
+  String altura = '';
+  String peso = '';
+  String habilidades = '';
+  String ataques = '';
+  String mensagemErro = '';
 
   void _procurar() async {
- String url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
-  
-  final response = await http.get(Uri.parse(url));
+    if (widget.controller.text.isEmpty) {
+      setState(() {
+        mensagemErro = 'Por favor, digite o nome de um Pokémon.';
+      });
+      return;
+    }
 
-  if (response.statusCode == 200) {
-    final dados =jsonDecode(response.body);
+    String url = 'https://pokeapi.co/api/v2/pokemon/${widget.controller.text.toLowerCase()}';
 
-  // Filtrando informações específicas 
-    String nome = dados['name'];
-    int altura = dados['height'];
-    int peso = dados['weight'];
-    List habilidades = dados['abilities'];
-    List Ataques = dados['moves'];
+    try {
+      final response = await http.get(Uri.parse(url));
 
-    // Organizando as habilidades
-    String habilidadesFormatadas = habilidades
+      if (response.statusCode == 200) {
+        final dados = jsonDecode(response.body);
 
-        .map((habilidade) => habilidade['ability']['name'])
-        .join(', ');
-    // Organizando os ataques
-
-    String ataquesFormatadas = Ataques
-    .map((ataque) => ataque['move']['name'])
-    .join(', ');
-
-  // Exibindo as informações no terminal
-  print('Nome: $nome');
-  print('Altura: $altura');
-  print('Peso: $peso');
-  print('Habilidades: $habilidadesFormatadas');
-  print('Ataques: $ataquesFormatadas');
-  } else {
-    // Se o servidor retornar um código de status diferente de 200, lança uma exceção.
-    throw Exception('Falha ao carregar os dados do Pokémon');
-  }
+        setState(() {
+          nome = dados['name'];
+          altura = '${dados['height']}';
+          peso = '${dados['weight']}';
+          habilidades = dados['abilities']
+              .map((habilidade) => habilidade['ability']['name'])
+              .join(', ');
+          ataques = dados['moves']
+              .take(5)
+              .map((ataque) => ataque['move']['name'])
+              .join(', ');
+          tipos = dados['types']
+              .map((tipo) => tipo['type']['name'])
+              .join(', ');
+          mensagemErro = ''; // Limpa a mensagem de erro
+        });
+      } else {
+        setState(() {
+          mensagemErro = 'Pokémon não encontrado.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        mensagemErro = 'Erro ao buscar os dados: $e';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
-              'Texto',
+              'Pokedex',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            TextField(
+              controller: widget.controller,
+              decoration: InputDecoration(
+                labelText: 'Digite o nome do Pokémon',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _procurar,
+              child: const Text('Procurar'),
+            ),
+            if (mensagemErro.isNotEmpty)
+              Text(
+                mensagemErro,
+                style: const TextStyle(color: Color.fromARGB(255, 231, 23, 8)),
+              ),
+            if (nome.isNotEmpty) ...[
+              Text('Nome: $nome'),
+              Text('Tipos: $tipos'),
+              Text('Altura: $altura'),
+              Text('Peso: $peso'),
+              Text('Habilidades: $habilidades'),
+              Text('Ataques: $ataques'),
+            ],
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _procurar,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
